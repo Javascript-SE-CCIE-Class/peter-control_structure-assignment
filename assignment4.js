3060c1171ae7057b5381fc52c220559e01185781
peterwathithi <peterwathithi8@gmail.com>
2018-11-28 16:16:45 +0300

    finall commit on control structures and edited read me file

---
+++ assignment4.js
@@ -0,0 +1,17 @@
+//answer to Given two numeric variables, a and b, write the following:
+//
+// Create a variable, add, and assign it the sum of a and b.
+// Create a variable, sub, and assign it result of b subtracted from a.
+// Create a variable, multi, and assign it the product of a and b.
+// Create a variable, div, and assign it the result of a divided by b .
+// Create a variable, inc, and assign it the preincremented value of a.
+// Create a variable, dec, and assign it the predecremented value of b.
+
+let a;
+let b;
+let add=a+b;
+let sub= b-a;
+let multi=a*b;
+let div=a/b;
+let inc=a++;
+let dec=++b;
